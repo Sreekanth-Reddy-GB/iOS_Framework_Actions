eb640924f476afc100fadaf9f75e76dc56387025
Sreekanth <sreekanthreddy@Sreekanths-MacBook-Air.local>
2022-03-17 15:23:09 +0530

    Initial commit

---
+++ FrameworkActionsDemo/FrameworkActionsDemo.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkActionsDemo.h
+//  FrameworkActionsDemo
+//
+//  Created by Sreekanth Reddy on 17/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkActionsDemo.
+FOUNDATION_EXPORT double FrameworkActionsDemoVersionNumber;
+
+//! Project version string for FrameworkActionsDemo.
+FOUNDATION_EXPORT const unsigned char FrameworkActionsDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkActionsDemo/PublicHeader.h>
+
+
---
+++ FrameworkActionsDemo/Test.swift
@@ -0,0 +1,9 @@
+//
+//  Test.swift
+//  FrameworkActionsDemo
+//
+//  Created by Sreekanth Reddy on 17/03/22.
+//
+
+import Foundation
+
